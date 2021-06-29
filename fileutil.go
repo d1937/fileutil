a4e45f2d7990b6dfb31fb5514ea39cdbaaab51f3
samy <samy1090900@outlook.com>
2021-06-29 22:25:44 +0800

    add WalkDir

--- fileutil.go
+++ fileutil.go
@@ -6,6 +6,8 @@ import (
 	"io/ioutil"
 	"net/http"
 	"os"
+	"path/filepath"
+	"strings"
 	"time"
 
 	"github.com/karrick/godirwalk"
@@ -164,4 +166,28 @@ func WriteContentToFile(content []byte, filepath string, flag int) error {
 		return err
 	}
 	return nil
+}
+
+//获取指定目录及所有子目录下的所有文件，可以匹配后缀过滤。
+func WalkDir(dirPth, suffix string) (files []string, err error) {
+	files = make([]string, 0, 30)
+	suffix = strings.ToUpper(suffix) //忽略后缀匹配的大小写
+
+	err = filepath.Walk(dirPth, func(filename string, fi os.FileInfo, err error) error { //遍历目录
+		//if err != nil { //忽略错误
+		// return err
+		//}
+
+		if fi.IsDir() { // 忽略目录
+			return nil
+		}
+
+		if strings.HasSuffix(strings.ToUpper(fi.Name()), suffix) {
+			files = append(files, filename)
+		}
+
+		return nil
+	})
+
+	return files, err
 }
