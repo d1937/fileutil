acc1d860b4c4e643ed713d224f3692a85257e27a
samy <samy1090900@outlook.com>
2021-07-05 16:46:32 +0800

    update WalkDir

--- fileutil.go
+++ fileutil.go
@@ -170,13 +170,13 @@ func WriteContentToFile(content []byte, filepath string, flag int) error {
 
 //获取指定目录及所有子目录下的所有文件，可以匹配后缀过滤。
 func WalkDir(dirPth, suffix string) (files []string, err error) {
-	files = make([]string, 0, 30)
+	files = make([]string, 0)
 	suffix = strings.ToUpper(suffix) //忽略后缀匹配的大小写
 
 	err = filepath.Walk(dirPth, func(filename string, fi os.FileInfo, err error) error { //遍历目录
-		//if err != nil { //忽略错误
-		// return err
-		//}
+		if err != nil { //忽略错误
+			return err
+		}
 
 		if fi.IsDir() { // 忽略目录
 			return nil
